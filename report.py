3ac7232546990fa86549e006451e9cb0a51b14b9
behnamfarhadi2010 <farhadi.behnam@gmail.com>
2021-05-31 10:43:26 -0230

    Exercise 2.11

--- report.py
+++ report.py
@@ -35,8 +35,34 @@ def read_prices(filename):
 
     return prices
 
+def make_report_data(portfolio, prices):
+    '''
+    Make a list of (name, shares, price, change) tuples given a portfolio list
+    and prices dictionary.
+    '''
+    rows = []
+    for stock in portfolio:
+        current_price = prices[stock['name']]
+        change = current_price - stock['price']
+        summary = (stock['name'], stock['shares'], current_price, change)
+        rows.append(summary)
+    return rows
+        
+# Read data files and create the report data        
+
 portfolio = read_portfolio('Data/portfolio.csv')
-prices    = read_prices('Data/prices.csv')
+prices = read_prices('Data/prices.csv')
+
+# Generate the report data
+
+report = make_report_data(portfolio, prices)
+
+# Output the report
+headers = ('Name', 'Shares', 'Price', 'Change')
+print('%10s %10s %10s %10s' % headers)
+print(('-' * 10 + ' ') * len(headers))
+for row in report:
+    print('%10s %10d %10.2f %10.2f' % row)
 
 # Calculate the total cost of the portfolio
 total_cost = 0.0
