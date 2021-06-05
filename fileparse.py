6982ffb1279f3ade623396a850fe9ca082fffb0a
behnamfarhadi2010 <farhadi.behnam@gmail.com>
2021-06-05 02:07:18 -0230

    Exercise 3.10

--- fileparse.py
+++ fileparse.py
@@ -2,16 +2,16 @@
 import csv
 
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
      
     if select and not has_headers:
-        raise RuntimeError("select argument requires column headers")
+        raise RuntimeError('select requires column headers')
   
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
 
         # Read the file headers
-        headers = next(rows)
+        headers = next(rows) if has_headers else []
 
         # If a column selector was given, find indices of the specified columns.
         # Also narrow the set of headers used for resulting dictionaries
@@ -22,13 +22,19 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
             indices = []
 
         records = []
-        for row in rows:
+        for rowno, row in enumerate (rows,1):
             if not row:   
                 
                 continue
             # Filter the row if specific columns were selected
-            if indices:
-                row = [ row[index] for index in indices ]
+            if select:
+                try:
+                    row = [func(val) for func, val in zip(types, row)]
+                except ValueError as e:
+                    if not silence_errors:
+                        print(f"Row {rowno}: Couldn't convert {row}")
+                        print(f"Row {rowno}: Reason {e}")
+                    continue
 
             # Make a dictionary
             if types:
@@ -37,7 +43,9 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
             if headers:
                 record = dict(zip(headers, row))
 
-            record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+
             records.append(record)
 
-    return records
+        return records
