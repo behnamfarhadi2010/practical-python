3de9406ef4b327cadf90bb01af5ffa80f4300735
behnamfarhadi2010 <farhadi.behnam@gmail.com>
2021-06-06 04:58:44 -0230

    Exercise 3.16

--- pcost.py
+++ pcost.py
@@ -1,16 +1,22 @@
+# pcost.py
+
 import report
-import csv
-def pcost(filename):
+
+def portfolio_cost(filename):
+    '''
+    Computes the total cost (shares*price) of a portfolio file
+    '''
     portfolio = report.read_portfolio(filename)
-    return sum([s['shares']*s['price'] for s in portfolio])
+    return sum([s['shares'] * s['price'] for s in portfolio])
 
-import sys
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    filename = input('Enter a filename:')
+def main(args):
+    if len(args) != 2:
+        raise SystemExit('Usage: %s portfoliofile' % args[0])
+    filename = args[1]
+    print('Total cost:', portfolio_cost(filename))
 
-cost = pcost(filename)
-print('Total cost:', cost)
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
 
 # pcost.py
